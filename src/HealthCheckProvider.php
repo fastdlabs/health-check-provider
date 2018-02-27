b83722b23fe3f7de6aea46efd5d10197adf82252
JanHuang <bboyjanhuang@gmail.com>
2018-02-27 11:05:25 +0800

    init

---
+++ src/HealthCheckProvider.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @author    jan huang <bboyjanhuang@gmail.com>
+ * @copyright 2018
+ *
+ * @see      https://www.github.com/janhuang
+ * @see      http://www.fast-d.cn/
+ */
+
+namespace FastD\HealthCheckProvider;
+
+
+use FastD\Container\Container;
+use FastD\Container\ServiceProviderInterface;
+
+class HealthCheckProvider implements ServiceProviderInterface
+{
+    /**
+     * @param Container $container
+     * @return mixed
+     */
+    public function register(Container $container)
+    {
+        route()->get('/health-check', function () {
+            return json([
+                'version' => version(),
+                'app' => app()->getName(),
+            ]);
+        });
+    }
+}
